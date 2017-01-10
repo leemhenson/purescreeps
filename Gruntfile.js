34aa88ebb2bcf9410a95276c6422b86d2a05b540
Lee Henson <lee.m.henson@gmail.com>
2017-01-10 20:59:28 +0000

    feat: added Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-screeps');
+
+  grunt.initConfig({
+    screeps: {
+      options: {
+        email: process.env.EMAIL,
+        password: process.env.PASSWORD,
+        branch: 'default',
+        ptr: false
+      },
+      dist: {
+        src: ['dist/*.js']
+      }
+    }
+  });
+}
