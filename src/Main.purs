a04f82e5702dbc9678e1bc6d3f1e04ddfa112e59
Lee Henson <lee.m.henson@gmail.com>
2017-01-16 20:47:29 +0000

    feat: basic logging of spawns and rooms

--- src/Main.purs
+++ src/Main.purs
@@ -1,10 +1,29 @@
 module Main where
 
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, logShow)
-import Prelude (Unit)
-import Screeps (CMD)
+import Control.Monad.Eff.Console (CONSOLE, log)
+import Data.Traversable (for_)
+import Prelude (bind, Unit)
+import Screeps.Effects (TICK)
+import Screeps.Game (getGameGlobal, rooms, spawns)
+import Screeps.Room (name) as Room
+import Screeps.Spawn (name) as Spawn
+import Screeps.Types (GameGlobal)
 
-loop :: ∀ e. Eff (cmd :: CMD, console :: CONSOLE | e) Unit
+spawnCreeps :: ∀ e. GameGlobal -> Eff (console :: CONSOLE | e) Unit
+spawnCreeps game = do
+  log "spawnCreeps"
+
+  for_ (spawns game) \s ->
+    log (Spawn.name s)
+
+loop :: ∀ e. Eff (tick :: TICK, console :: CONSOLE | e) Unit
 loop = do
-  logShow "testing"
+  log "loop"
+
+  game <- getGameGlobal
+
+  for_ (rooms game) \r ->
+    log (Room.name r)
+
+  spawnCreeps game
