7d702b78363960dd92574a7c3c99d2d4c55e5942
Lee Henson <lee.m.henson@gmail.com>
2017-01-18 21:22:58 +0000

    feat: add spawning info to spawn log msg

--- src/Main.purs
+++ src/Main.purs
@@ -3,21 +3,30 @@ module Main where
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Int (decimal, toStringAs)
+import Data.Maybe (maybe)
 import Data.Traversable (for_)
 import Prelude ((<>), bind, Unit)
 import Screeps.Effects (TICK)
 import Screeps.Game (getGameGlobal, rooms, spawns)
 import Screeps.Room (name)
 import Screeps.Room (name) as R
+import Screeps.Spawn (CreepInfo)
 import Screeps.Spawn (energy, energyCapacity, name) as S
 import Screeps.Types (GameGlobal, Room, Spawn)
 
+showCreepInfo :: CreepInfo -> String
+showCreepInfo info = info.name <> " (" <> info.remainingTime <> "/" <> info.needTime <> ")"
+
+showSpawningCreepInfo :: CreepInfo -> String
+showSpawningCreepInfo info = " spawning " <> showCreepInfo info
+
 showSpawn :: Spawn -> String
-showSpawn spawn = "[spawn] " <> name <> " (" <> energy <> "/" <> capacity <> ")"
+showSpawn spawn = "[spawn] " <> name <> " (" <> energy <> "/" <> capacity <> ")" <> spawning
   where
     capacity = toStringAs decimal (S.energyCapacity spawn)
     energy = toStringAs decimal (S.energy spawn)
     name = S.name spawn
+    spawning = maybe "" showSpawningCreepInfo (S.spawning spawn)
 
 showRoom :: Room -> String
 showRoom room = "[room] " <> name
